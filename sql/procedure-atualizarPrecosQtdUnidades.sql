5361fe97b2e05b792b732baa3935e1f97ab64c9e
Wesley Lopes <nobretecnologia@gmail.com>
2019-04-30 10:34:20 -0300

    backup  30-04-2019 10:34

--- sql/procedure-atualizarPrecosQtdUnidades.sql
+++ sql/procedure-atualizarPrecosQtdUnidades.sql
@@ -1,32 +1,35 @@
- DROP PROCEDURE IF EXISTS  atualizarPrecosQtdUnidades;
+ DROP PROCEDURE IF EXISTS  buscaPrecoQtdUnidade;
 DELIMITER $$
-CREATE PROCEDURE atualizarPrecosQtdUnidades( 
+CREATE PROCEDURE buscaPrecoQtdUnidade( 
    IN p_unidade  VARCHAR(70) COLLATE utf8_unicode_ci,
-   -- IN p_dataInicial DATE ,
-   -- IN p_dataFinal DATE, 
-   IN p_tipoCombustivel VARCHAR(70)COLLATE utf8_unicode_ci)
+   IN p_tipoCombustivel VARCHAR(70)COLLATE utf8_unicode_ci,
+   IN p_dataInicial VARCHAR(70)COLLATE utf8_unicode_ci,
+   IN p_dataFinal VARCHAR(70)COLLATE utf8_unicode_ci )
+   
 BEGIN
   DECLARE somaQuantidade FLOAT DEFAULT 0;
   DECLARE peso FLOAT DEFAULT 0;  /* ( quantidade /  somaQuantidade)*/ 
   DECLARE valorCombustivelPeso FLOAT DEFAULT 0; /* ( valor unitário *  Peso )*/
   DECLARE somaValorCombustivel FLOAT DEFAULT 0; /* ( )*/
+  DECLARE qtdCombustivel VARCHAR(70) DEFAULT '';
   DECLARE existe_mais_linhas INT DEFAULT 0;
   DECLARE v_centroResultado VARCHAR(70) DEFAULT '';
   DECLARE v_valorUnitario FLOAT DEFAULT 0;
   DECLARE v_quantidade FLOAT DEFAULT 0;
   DECLARE v_data_movimento DATETIME;
   DECLARE v_cidade VARCHAR(70) DEFAULT '';
+  DECLARE v_produto VARCHAR(70) DEFAULT '';
   DECLARE p_comb VARCHAR(70) DEFAULT '';
   
   DECLARE cursorMovimentoVeiculos CURSOR FOR SELECT DISTINCT(CENTRO_RESULTADO),
 								    VALOR_UNITARIO,
                                     QUANTIDADE,
                                     DATA_MOVIMENTO,
-                                    CIDADE
+                                    CIDADE,PRODUTO 
 							      FROM movimento_veiculos a 
 	                              WHERE a.PRODUTO like CONCAT('%', p_tipoCombustivel, '%')
 							        AND a.CENTRO_RESULTADO = p_unidade
-						            AND  DATE(a.DATA_MOVIMENTO) between '2019-04-23' and '2019-04-23';  
+						            AND  DATE(a.DATA_MOVIMENTO) between p_dataInicial and p_dataFinal;  
                                
     -- Definição da variável de controle de looping do cursor                             
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET existe_mais_linhas=1;
@@ -37,7 +40,7 @@ BEGIN
 					    FROM movimento_veiculos b 
 	                    WHERE b.PRODUTO like CONCAT('%', p_tipoCombustivel, '%')
 						AND b.CENTRO_RESULTADO =p_unidade 
-						AND DATE(b.DATA_MOVIMENTO) between '2019-04-23' and '2019-04-23');  
+						AND DATE(b.DATA_MOVIMENTO) between p_dataInicial and p_dataFinal);  
 
 -- Abertura do cursor
   OPEN cursorMovimentoVeiculos;
@@ -48,7 +51,8 @@ BEGIN
                                       v_valorUnitario,
                                       v_quantidade, 
                                       v_data_movimento, 
-                                      v_cidade;
+                                      v_cidade,
+                                      v_produto;
 
   -- Controle de existir mais registros na tabela
   IF existe_mais_linhas = 1 THEN
@@ -61,19 +65,14 @@ BEGIN
 
   -- Retorna para a primeira linha do loop
   END LOOP cursorMovimentoVeiculos;
-
+  
+  SET qtdCombustivel = IF (ROUND(somaQuantidade)>1000,REPLACE(FORMAT(ROUND(somaQuantidade),0), ',', '.'), CONCAT("0.", round(somaQuantidade)));
   -- Setando a variável com o resultado final
-
-SELECT v_centroResultado as CENTRO_RESULTADO ,
-round(somaValorCombustivel,2) as VALOR_COMBUSTIVEL_PONDERADO, 
-ROUND(somaQuantidade) as SOMA_QUANTIDADE,
-v_centroResultado,
-v_valorUnitario,
-v_quantidade, 
-v_data_movimento, 
-v_cidade,
-p_unidade;
-  END $$
+  
+  SELECT v_centroResultado as CENTRO_RESULTADO ,
+    ROUND(somaValorCombustivel,2) as VALOR_COMBUSTIVEL, 
+	qtdCombustivel as QUANTIDADE_LITROS, v_produto AS TIPO_COMBUSTIVEL, p_tipoCombustivel AS TIPO_COMBUSTIVEL_BUSCA; 
+END $$
 
 
 
